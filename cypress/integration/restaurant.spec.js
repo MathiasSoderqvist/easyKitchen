fd511b38fc0a90a6ac7cc7d174b0fd274faaed63
ascolm <mertascioglu@gmail.com>
2021-03-18 10:02:18 +0100

    test(cypress): disable db related imports that were causing errors in runtime

--- cypress/integration/restaurant.spec.js
+++ cypress/integration/restaurant.spec.js
@@ -3,8 +3,6 @@
 // ******* TODO: CURRENTLY DOESN'T ENFORCE RUNNING THE APP IN TEST MODE **********
 
 const { testDishes, testMenuTitle } = require('./mocks')
-const { runServer } = require('../../server');
-const { Dish, Model } = require('../../server/models');
 
 describe(`Creating a new dish: ${testDishes[0].title} `, () => {
   it('visits the Happy Kitchen app', () => {
